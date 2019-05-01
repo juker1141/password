1d1f018a9c6626d59d1774cb423e4a1c5bafcd9e
Juker1141 <juker1141@gmail.com>
2019-05-01 20:47:48 +0800

    Upadte something

--- password.py
+++ password.py
@@ -1,14 +1,14 @@
 password = 'a123456'
-x = 3
 y = 3
-while x > 0:
+while y > 0:
+	y = y - 1
 	ps = input('請輸入密碼: ') 
 	if ps == password:
 		print('登入成功!')
 		break
 	else:
-		y = y - 1
+		if y == 0:
+			print('帳戶已遭封鎖!')
+			break
 		print('密碼錯誤!還有' ,y ,'次機會')
-	x = x - 1
-if y == 0:
-	print('帳戶已遭封鎖!')
+		
